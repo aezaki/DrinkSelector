038d6b130056f89950674c5fe2027af4eafa22ab
Andrew Zaki <aezaki@edu.uwaterloo.ca>
2021-08-10 12:05:42 -0700

    Initial Commit

---
+++ Drink Selecter/Drink_SelecterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drink_SelecterApp.swift
+//  Drink Selecter
+//
+//  Created by Andrew Z on 2021-08-10.
+//
+
+import SwiftUI
+
+@main
+struct Drink_SelecterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
